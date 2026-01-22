e5cf8e074670e317793e2f321678ff2597538e36
Cecep Yunar <pepetocorleone@gmail.com>
2026-01-22 17:04:43 +0700

    route sample

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import ListPage from './pages/ListPage';
-import ProtectedRoute from './components/Layout/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route 
-          path="/home" 
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/list" 
-          element={
-            <ProtectedRoute>
-              <ListPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route path="*" element={<Navigate to="/home" />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<HomePage />} />
+        <Route path="/admin" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
